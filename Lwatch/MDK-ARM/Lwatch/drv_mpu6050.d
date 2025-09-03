3e9553a9c0aa9d1ab48752b90ec995e6260b82d1
RoadToSea <3376714571@qq.com>
2025-09-03 11:17:07 +0800

    [fix] update gitignore

--- Lwatch/MDK-ARM/Lwatch/drv_mpu6050.d
+++
@@ -1,6 +0,0 @@
-lwatch\drv_mpu6050.o: ..\driver\drv_mpu6050.c
-lwatch\drv_mpu6050.o: ..\driver\drv_mpu6050.h
-lwatch\drv_mpu6050.o: ..\driver\drv_manager.h
-lwatch\drv_mpu6050.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdio.h
-lwatch\drv_mpu6050.o: ..\driver\drv_base.h
-lwatch\drv_mpu6050.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
